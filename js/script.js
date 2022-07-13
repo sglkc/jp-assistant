20b77f009a06cac9056100f81f6cd151d4b6ab33
sglkc <31957516+sglkc@users.noreply.github.com>
2022-07-13 22:33:56 +0700

    ✨ add verb deconjugator

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-const Wanakana = require('wanakana');
-const Deconjugator = require('jp-verbs');
-const Kuroshiro = require('kuroshiro');
-const KuromojiAnalyzer = require('kuroshiro-analyzer-kuromoji');
-const kuroshiro = new Kuroshiro();
+window.Wanakana = require('wanakana');
+window.Deconjugator = require('jp-verbs');
+window.Kuroshiro = require('kuroshiro');
+window.KuromojiAnalyzer = require('kuroshiro-analyzer-kuromoji');
+window.kuroshiro = new Kuroshiro();
 
-/* Input Method Editor */
-const ime = document.getElementById('wanakana');
-
-Wanakana.bind(ime);
+/* Bind Input Method Editor */
+$('.wanakana').each((i, elm) => {
+  Wanakana.bind(elm);
+});
 
 /* Copy button listener */
 $('.btn-copy').click(function () {
@@ -16,11 +16,68 @@ $('.btn-copy').click(function () {
 
   element.select();
   element.setSelectionRange(0, 999);
- // navigator.clipboard.writeText(element.value);
+  navigator.clipboard.writeText(element.value);
 
   this.innerText = 'Copied!';
 
   setTimeout(() => {
     this.innerText = 'Copy';
   }, 3000);
+});
+
+/* Deconjugator function */
+function deconjugate() {
+  const verb = new URLSearchParams(location.search).get('deconjugate');
+  const template = $('#deconjugator-template')
+    .clone()
+    .attr('id', null)
+    .removeClass('d-none');
+
+  $('#deconjugator-list > :not(#deconjugator-template)').remove();
+
+  Deconjugator.unconjugate(verb).forEach((word) => {
+    $('.card-header', template).text(word.base)
+      .attr('href', 'https://jisho.org/search/' + word.base);
+
+    const forms = word.derivationSequence.wordFormProgression;
+    const links = Deconjugator.GrammarLinkForWordType;
+
+    word.derivationSequence.derivations.forEach((derivation, i) => {
+      const clickable =
+        links[derivation]
+        ? `<a href="${links[derivation]}">${derivation}</a>`
+        : derivation;
+
+      $('ul', template).append(
+        `<li class="list-group-item py-3">${forms[i]}<br>${clickable}</li>`
+      );
+    });
+
+    if (!forms.length) {
+      $('ul', template).append(
+        `<li class="list-group-item">Already at dictionary form</li>`
+      );
+    }
+
+    template.clone().appendTo('#deconjugator-list');
+    $('ul > li', template).remove();
+  });
+}
+
+/* Deconjugate if found parameter */
+if (new URLSearchParams(location.search).get('deconjugate')) {
+  const verb = new URLSearchParams(location.search).get('deconjugate');
+
+  $('[name="deconjugate"').val(verb);
+  document.getElementById('deconjugator').scrollIntoView({ block: 'center' });
+  deconjugate();
+}
+
+/* Deconjugator submit */
+$('#deconjugator > form').submit((e) => {
+  const verb = $('[name="deconjugate"]').val();
+
+  e.preventDefault();
+  history.replaceState(null, "", '?deconjugate=' + verb);
+  deconjugate();
 });
