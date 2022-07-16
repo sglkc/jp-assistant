a51592b53026f89b2b1522ca2fee3228593bb9d4
sglkc <31957516+sglkc@users.noreply.github.com>
2022-07-16 23:27:24 +0700

    ✨ more converter settings

--- js/script.js
+++ js/script.js
@@ -114,48 +114,60 @@ $('#deconjugator form').submit((e) => {
 });
 
 /* Init Kuroshiro */
-$('#convert .textarea-container button').click(() => {
-  $('#convert-result').removeClass('text-danger');
-  $('#convert-result').html('<div class="spinner-border"></div>');
-  $('#convert .textarea-container button').addClass('d-none');
+$('#converter .textarea-container button').click(() => {
+  $('#converter-result').removeClass('text-danger');
+  $('#converter-result').html('<div class="spinner-border"></div>');
+  $('#converter .textarea-container button').addClass('d-none');
 
   kuroshiro.init(new KuromojiAnalyzer())
     .then(() => {
-      $('#convert-result').html('Result will be shown here');
-      $('#convert textarea, #convert-settings :not(:last-child)')
+      $('#converter-result').html('Result will be shown here');
+      $('#converter-result').css('white-space', 'pre-wrap');
+      $('#converter textarea, [disabled]:not([name="romajiSystem"], [name="rt"])')
         .attr('disabled', false);
     })
     .catch((e, a) => {
-      $('#convert .textarea-container button').removeClass('d-none');
-      $('#convert-result').html('Unexpected error: ' + e);
-      $('#convert-result').addClass('text-danger');
+      $('#converter .textarea-container button').removeClass('d-none');
+      $('#converter-result').html('Unexpected error: ' + e);
+      $('#converter-result').addClass('text-danger');
     });
 });
 
 /* Converter generate function */
 function convert() {
-  const text = $('#convert textarea').val();
+  const text = $('#converter textarea').val();
 
   kuroshiro.convert(text, kuroshiroConfig)
     .then((result) => {
       const display = result || 'Result will be shown here';
+      const furiganaSize = $('#converter-settings [name="rt"]').val();
 
-      $('#convert-result').html(display);
+      $('#converter-result').html(display);
+      $('#converter-result rt').css('font-size', furiganaSize + 'px');
     });
 }
 
 /* Converter auto-submit */
-$('#convert textarea').on('input', convert);
+$('#converter textarea').on('input', convert);
 
 /* Converter settings */
-$('#convert-settings select').change(function () {
+$('#converter-settings select').change(function () {
   const name = this.name;
   const option = this.value.toLowerCase();
 
   kuroshiroConfig[name] = option;
 
   const isRomaji = kuroshiroConfig.to === 'romaji';
+  const isFurigana = kuroshiroConfig.mode === 'furigana';
 
-  $('#convert-settings [name="romajiSystem"]').attr('disabled', !isRomaji);
+  $('#converter-settings [name="romajiSystem"]').attr('disabled', !isRomaji);
+  $('#converter-settings [name="rt"]').attr('disabled', !isFurigana)
   convert();
+});
+
+$('#converter-settings input[type="range"]').on('input', function () {
+  const name = this.name;
+  const value = this.value;
+
+  $('#converter-result ' + name).css('font-size', value + 'px');
 });
