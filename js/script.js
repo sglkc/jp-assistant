01584a3795446fe0d6589766bc78e96551c48560
sglkc <31957516+sglkc@users.noreply.github.com>
2022-07-20 19:48:59 +0700

    ✨ add verb conjugator

--- js/script.js
+++ js/script.js
@@ -47,6 +47,68 @@ $('.btn-copy').click(function () {
   }, 3000);
 });
 
+/* Init conjugator */
+{
+  const template = $('#conjugator-template')
+    .clone()
+    .attr('id', null)
+    .removeClass('d-none');
+
+  $('#conjugator-template').remove();
+  $('input', template).attr('name', 'conjugation');
+  $('input', template).attr('type', 'radio');
+  window.Kamiya.conjugations.forEach((con) => {
+    $('label', template).text(con);
+    $('label', template).attr('for', con);
+    $('input', template).attr('id', con);
+    $('input', template).attr('value', con);
+
+    template.clone().appendTo('#conjugator-settings > :nth-child(2)')
+  });
+
+  $('input', template).attr('name', 'auxilaries[]');
+  $('input', template).attr('type', 'checkbox');
+  $('input', template).attr('required', null);
+  window.Kamiya.auxiliaries.forEach((aux) => {
+    $('label', template).text(aux);
+    $('label', template).attr('for', aux);
+    $('input', template).attr('id', aux);
+    $('input', template).attr('value', aux);
+
+    template.clone().appendTo('#conjugator-settings > :nth-child(3)')
+  });
+}
+
+/* Conjugator submit */
+$('#conjugator form').submit((e) => {
+  const verb = $('[name="conjugate"]').val();
+  const conjugation = $('[name="conjugation"]:checked').val();
+  const ichidan = $('[name="ichidan"]').is(':checked');
+  const auxilaries = [];
+
+  $('[name="auxilaries[]"]:checked').each((i, e) => {
+    auxilaries.push(e.value);
+  });
+
+  if (!verb && !conjugation) return e.reportValidity();
+
+  e.preventDefault();
+  $('#conjugator-results').removeClass('d-none');
+  try {
+    $('#conjugator-results').children().remove();
+    window.Kamiya.conjugateAuxiliaries(verb, auxilaries, conjugation, ichidan)
+      .forEach((conjugated) => {
+        $('#conjugator-results').append(
+          `<li class="list-group-item list-group-item-info">${conjugated}</li>`
+        );
+      });
+  } catch (error) {
+    $('#conjugator-results').append(
+      `<li class="list-group-item list-group-item-danger">${error}</li>`
+    );
+  }
+});
+
 /* Deconjugator function */
 function deconjugate() {
   const verb = new URLSearchParams(location.search).get('deconjugate');
